a9321d4b05d7f564ef983db1d34fce6a9e382c89
nikthequik <quik.the.nick@gmail.com>
2019-05-14 21:11:07 -0400

    -add articles and projects section -add new pictures -enhance mobile design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ProjectsComponent } from './projects/projects.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArticlesComponent } from './articles/articles.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'articles', component: ArticlesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    ProjectsComponent,
+    HomeComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
